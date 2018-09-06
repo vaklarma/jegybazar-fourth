17acc10e64becfb848646472e354e1fea035c0fa
vaklarma <biro.andrasgabor.regist@gmail.com>
2018-09-06 21:23:54 +0200

    Első komit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jegybazar-fourth';
+  title = 'jegybazar-fourth egy újabb';
 }
